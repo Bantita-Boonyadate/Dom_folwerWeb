7662857f927fde729d8c7c3eddf0f172e51aed03
Bantita Boonyadate <bantita_boonyadate@cmu.ac.th>
2021-06-15 22:51:32 +0700

    commit add footer
    add bootstrap footer

--- script.js
+++ script.js
@@ -9,22 +9,18 @@ function displayDetails(){
     let img = document.createElement('img')
     img.src = pic
     img.setAttribute('id','pic')
-    
-    if(!name || !des || !pic){
-        alert('Please fill all the boxes')
-        return
-    }
+    let price = document.getElementById('price').value
 
     let newRow = display.insertRow(row)
     let cell1 = newRow.insertCell(0)
     let cell2 = newRow.insertCell(1)
     let cell3 = newRow.insertCell(2)
+    let cell4 = newRow.insertCell(3)
 
     cell1.innerHTML = name
     cell2.innerHTML = des
     cell3.appendChild(img)
-
-
+    cell4.innerHTML = price
     row++
 }
 
